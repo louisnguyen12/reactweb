b6f5ace8fa1dfb6c176a019e688411bf1e20cd86
Louis Nguyen <45759180+louisnguyen12@users.noreply.github.com>
2019-05-13 12:06:05 +0700

    structure rearranged

---
+++ src/components/Cart.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+class Cart extends Component {
+    render() {
+      return (
+        <div className="App">
+          
+        </div>
+      )
+    }
+  }
+  
+  export default Cart;
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../css/Footer.css';
+
+class Footer extends Component {
+    render() {
+      return (
+        <div className="App">
+          
+        </div>
+      )
+    }
+  }
+  
+  export default Footer;
